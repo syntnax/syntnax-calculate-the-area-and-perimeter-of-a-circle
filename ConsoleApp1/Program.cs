3851caf3b4cce2f0b6fbac2152fdbde1b6f18b24
harun <harun@HARRRRRUUUUNNNN>
2025-02-05 03:23:11 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const double pi = Math.PI; // Sabit değer tanımı
+        double alan, cevre, yaricap;
+
+        Console.WriteLine("Dairenin yarıçapını giriniz:");
+
+        string yaricapGirilen = Console.ReadLine();
+
+        
+        if (double.TryParse(yaricapGirilen, out yaricap))
+        {
+            cevre = 2 * pi * yaricap;
+            alan = pi * Math.Pow(yaricap, 2);
+
+            Console.WriteLine("Dairenin alanı: {0}", alan);
+            Console.WriteLine("Dairenin çevresi: {0}", cevre);
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz giriş. Lütfen bir sayı girin.");
+        }
+    }
+}
